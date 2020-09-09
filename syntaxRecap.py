96769a8b52501aca4dd151a5337166f88761c36a
Jitgitbit <rowowolf@gmail.com>
2020-09-09 20:36:26 +0200

    dynamic typing and restrictions on data type naming

--- syntaxRecap.py
+++ syntaxRecap.py
@@ -1,6 +1,7 @@
 #Python syntax recap
-
+#===================
 print('variables and constants...')
+
 x = 5 
 y = 3
 z = 7
@@ -30,3 +31,17 @@ print(' ((1 + 2) * 3) ** 4 / 5 - 6 = ')
 print( ((1 + 2) * 3) ** 4 / 5 - 6)
 
 print('--------------------------')
+print('dynamic typing...')
+
+b = "Alex"
+b = "blah"
+b = 1234
+print("b = ")
+print(b)
+
+print('--------------------------')
+print('restrictions on data types naming...')
+
+print("a-z, A-Z, 0-9, and _ are allowed")
+print("@ and % are not allowed")
+print("numbers can not be placed in the beginning of the name!")

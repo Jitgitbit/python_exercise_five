2216f7bae947b6211957c6a49fea677a405fdcae
Jitgitbit <rowowolf@gmail.com>
2020-10-08 20:26:33 +0200

    operatorsRecap started

---
+++ operatorsRecap.py
@@ -0,0 +1,15 @@
+#Operators in py recap
+#===================
+print( )
+print("Types of operators in py")
+print('==========================')
+
+print('arythmetic operators')
+print('assignment operators')
+print('comparison operators')
+print('logical operators')
+print('identity operators')
+print('membership operators')
+print('bitwise operators')
+
+print('--------------------------')

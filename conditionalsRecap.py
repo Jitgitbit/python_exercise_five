25313fbee88a1daec93ac6d211ae4a03b7191974
Jitgitbit <rowowolf@gmail.com>
2020-10-15 19:46:08 +0200

    Conditionals - If-else

---
+++ conditionalsRecap.py
@@ -0,0 +1,15 @@
+#Conditionals in Python recap
+#============================
+print( )
+print("If-else conditions in python...")
+print('==========================')
+x = 3
+y = 4
+
+if(x == y):
+	z = x + y
+	print(z)
+else:
+	print("They are not equal")
+
+print('--------------------------')

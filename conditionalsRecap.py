a30198fbd5a03553fe84c0981f950080d0fe77df
Jitgitbit <rowowolf@gmail.com>
2020-10-15 19:50:40 +0200

    Conditionals - elif condition

--- conditionalsRecap.py
+++ conditionalsRecap.py
@@ -13,3 +13,17 @@ else:
 	print("They are not equal")
 
 print('--------------------------')
+print( )
+print("El-if conditions in python...")
+print('==========================')
+x = 5
+y = 4
+
+if(x == y):
+	print("x and y are equal")
+elif(x > y):
+	print("x is greater than y")
+else:
+	print("They are not equal, x is smaller than y")
+
+print('--------------------------')

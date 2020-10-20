e8b684e70977d80302a0a3e32a28e26885f531b6
Jitgitbit <rowowolf@gmail.com>
2020-10-20 15:56:03 +0200

    For loops in Phyton

---
+++ loopsRecap.py
@@ -0,0 +1,15 @@
+#Loops in Python recap
+#============================
+print( )
+print("For loops in python...")
+print('==========================')
+variable = ['']
+for value in variable:
+	pass
+
+x = [1,2,3,4,5,6,7,8,9,10]
+
+for item in x:
+	print(item + 111)
+
+print('--------------------------')

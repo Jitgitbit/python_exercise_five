981cbd8517247a07fc265cdb89e0d06f53b266a9
Jitgitbit <rowowolf@gmail.com>
2020-10-21 12:33:34 +0200

    Loops and user input in Python

--- loopsRecap.py
+++ loopsRecap.py
@@ -13,3 +13,15 @@ for item in x:
 	print(item + 111)
 
 print('--------------------------')
+print( )
+print("For loops with user input in python...")
+print('==========================')
+
+x = [0,1,2,3,4,5,6,7,8,9]
+
+for i in x:
+	x[i] = input("Enter your name please: ")
+
+print("The new list of names is: ")
+print(x)
+print('--------------------------')

723f82db50c9b2538512852c1521f00823c05bb0
Jitgitbit <rowowolf@gmail.com>
2020-11-02 11:09:08 +0100

    dates in Python (cleaner)

--- dateRecap.py
+++ dateRecap.py
@@ -1,5 +1,9 @@
 import datetime
-
+#Dates in Python recap
+#============================
+print( )
+print("dates in python...")
+print('==========================')
 x = datetime.datetime.now()
-
 print(x)
+print('--------------------------')

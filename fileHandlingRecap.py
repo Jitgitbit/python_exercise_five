75d5c487e9590659236ab8798b10d68f9d878d58
Jitgitbit <rowowolf@gmail.com>
2020-11-02 12:13:24 +0100

    FileHandling in Python

---
+++ fileHandlingRecap.py
@@ -0,0 +1,8 @@
+#FileHandling in Python recap
+#============================
+print( )
+print("FileHandling in python...")
+print('==========================')
+print('Always remember to close a file in the end!')
+print('https://www.tutorialspoint.com/python3/python_files_io.htm')
+print('--------------------------')
